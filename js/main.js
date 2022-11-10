ef6572674ad24f1ac15ef46aafce568006bf7451
VARDAN <muradyan94vardan@gmail.com>
2022-11-11 00:15:54 +0400

    load 2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
-function openNav() {
-    document.getElementById("myNav").style.left = "0";
-}
+// function openNav() {
+//     document.getElementById("myNav").style.left = "0";
+// }
+//
+// function closeNav() {
+//     document.getElementById("myNav").style.left = "+100%";
+// }
 
-function closeNav() {
-    document.getElementById("myNav").style.left = "+100%";
+function toggleSideBarClassName() {
+    const sideBarEl = document.getElementById("myNav");
+    sideBarEl.classList.toggle("menu__active");
 }
